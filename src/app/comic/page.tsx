ca40203cdfe9ca24d97cc7de82b143b008195d47
vwl2 <vwl1415@rit.edu>
2024-06-13 13:00:11 -0400

    got most local storage down? figuring out  how to update page

--- src/app/comic/page.tsx
+++ src/app/comic/page.tsx
@@ -15,19 +15,19 @@ const Read = ({ firstPanelImage = baseFirstPanel, secondPanelImage = baseSecondP
     return (<>
         <div id="comic-panels">
             <div className="first-panel">
-                <Image id="first-img" src={firstPanelImage} alt="" width={900} height={600} />
+                <Image id="first-img" src={firstPanelImage} alt="" width={900} height={600}/>
             </div>
             <div className="second-panel">
-                <Image id="second-img" src={secondPanelImage} alt="" width={900} height={600} />
+                <Image id="second-img" src={secondPanelImage} alt="" width={900} height={600}/>
             </div>
             <div className="third-panel">
-                <Image id="third-img" src={thirdPanelImage} alt="" width={900} height={600} />
+                <Image id="third-img" src={thirdPanelImage} alt="" width={900} height={600}/>
             </div>
         </div>
         <div id="branch hooks">
-            <button id="first-branch-hook" onClick={updatePanelSet}>Branch 1</button>
-            <button id="second-branch-hook" onClick={updatePanelSet}>Branch 2</button>
-            <button id="third-branch-hook" onClick={updatePanelSet}>Branch 3</button>
+            <button id="first-branch-id" className="first-branch-hook" onClick={updatePanelSet}>Branch 1</button>
+            <button id="second-branch-id" className="second-branch-hook" onClick={updatePanelSet}>Branch 2</button>
+            <button id="third-branch-id" className="third-branch-hook" onClick={updatePanelSet}>Branch 3</button>
         </div>
         <button id="back-button">Back</button>
     </>);
--- src/scripts/Read.js
+++ src/scripts/Read.js
@@ -1,17 +1,38 @@
+"use client"
+
+//
+const storeName = "cur_uuid"
+
 //runs when a branch hook button is selected
-//should take in new
-const updatePanelSet = () => {
+const updatePanelSet = (branchHookId) => {
     //get data related with branch hook selected
-    //loadPanelSetJSON();
+    obj = readFromLocalStorage()[branchHookId];
+
+    //if error reading, log out error message
+    if (obj.error) {
+        let msg = obj.error;
+        console.log(msg);
+        return;
+    }
+
+    //runs if there is no data in branch (go to create)
+    if (!obj.data || obj.data.length == 0) {
+        //runs create page
+        return;
+    }
+    let branchData = obj.data;
 
     //loads in new panels for images
+    //sets the src of each image to new src
     
-    //puts in new branch hooks location (using x,y)
+    //puts in new branch hooks location (using x,y, and pan)
+    //
 
     //set up branch hooks to have childBranch id
+    //change id of correlating branch hook buttons 
 
-    //
 }
+
 //ex of structure
 // cur_uuid: {
 //     parentBranchId: "uuid",
@@ -46,20 +67,22 @@ const updatePanelSet = () => {
 //     ]
 //   };
 
-//parses out json correlated with branchId
-const readPanelSetJSON = (branchId) => {
-    //     const json = //     const string = localStorage.getItem(storeName);
-    //     let json;
-    //     try {
-    //         json = JSON.parse(string);
-    //         if (!json) throw new Error("json is null!");
-    //     } catch (error) {
-    //         console.log(`ERROR: ${error} with string: ${string}`);
-    //         json = {};
-    //     }
-    //     return json;
-    //     console.log(`Calling readFromLocalStorage(${key}) with value=${json[key]}`);
-    //     return json[key];
+//parses out json correlated with current branch id
+export const readFromLocalStorage = () => {
+    const string = localStorage.getItem(storeName);
+
+    let json;
+    try {
+        json = JSON.parse(string);
+        if (!json) throw new Error("json is null!");
+    } catch (error) {
+        console.log(`ERROR: ${error} with string: ${string}`);
+        json = {};
+    }
+    //test
+    console.log(`Calling readFromLocalStorage(${key}) with value=${json[key]}`);
+
+    return json;
 }
 
 export { updatePanelSet }
